5bd20a7671f8f472b28658114321c0e94ef0cced
Neuille-hush <ismetbeljulji@gmail.com>
2026-08-17 00:11:21 +0200

    MessageDao.kt

---
+++ app/src/main/java/com/ismet/novafr/data/MessageDao.kt
@@ -0,0 +1,19 @@
+package com.ismet.novafr.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MessageDao {
+
+    @Query("SELECT * FROM messages ORDER BY timestamp ASC")
+    fun getAllMessages(): Flow<List<Message>>
+
+    @Insert
+    suspend fun insert(message: Message): Long
+
+    @Query("DELETE FROM messages")
+    suspend fun clearHistory()
+}
